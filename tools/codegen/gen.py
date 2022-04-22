202336cbef6cb826b70193a028ef455a9c471b8a
Priya Ramani <priyaramani@fb.com>
2022-04-21 17:04:31 -0700

    [Pytorch][4/4 Static dispatch] Support multiple backends with multiple kernels
    - Supports multiple backends with multiple kernels in static dispatch
    - Refactor static dispatch generators
    
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/76059
    ghstack-source-id: 154524522
    
    Differential Revision: [D35727473](https://our.internmc.facebook.com/intern/diff/D35727473/)

--- tools/codegen/gen.py
+++ tools/codegen/gen.py
@@ -301,28 +301,25 @@ def static_dispatch_extra_headers(
     return [f'#include <ATen/{dispatch_key}Functions.h>'
             for dispatch_key in static_dispatch_keys(backends)]
 
-def generate_static_dispatch(
+# Translates DispatcherSignature to CppSignature arguments with support for SpecialArgName.possibly_redundant_memory_format
+# as tools.codegen.api.translate() doesn't support translation of arguments from DispatcherAPI->CppAPI when
+# there is a memory_format argument after TensorOption arguments
+def translate_args_disp_to_cpp(
     f: NativeFunction,
-    sig: DispatcherSignature, *,
-    method: bool,
-    backend_index: Optional[BackendIndex]
+    sig: DispatcherSignature
 ) -> str:
-    if backend_index is None or f.manual_kernel_registration:
-        return ""
     target_sig = CppSignatureGroup.from_native_function(f, method=False, fallback_binding=False).signature
-    name = target_sig.name()
 
     dp_sig_args = []
-
-    # TranslateAPI doesn't support translation of operators from DispatcherAPI->CppAPI when
-    # there is a memory_format argument after TensorOption arguments. For operators with such arguments,
-    # amend the dispatcher signature's memory_format argument to have the same nctype as the CPP signature
+    spl_mem_format_type = SpecialArgName.possibly_redundant_memory_format
     if len(target_sig.arguments()) > 0 and \
-            target_sig.arguments()[-1].nctype.name == SpecialArgName.possibly_redundant_memory_format:
+            target_sig.arguments()[-1].nctype.name == spl_mem_format_type:
+        # When target(CPP) signature's last argument is SpecialArgName.possibly_redundant_memory_format argument,
+        # amend the dispatcher signature's last argument(memory_format) to have the same nctype as the CPP signature
         last_disp_arg = sig.arguments()[-1]
         dp_sig_args = sig.arguments()[:-1]
         mem_format_arg = Binding(
-            nctype=NamedCType(SpecialArgName.possibly_redundant_memory_format, last_disp_arg.nctype.type),
+            nctype=NamedCType(spl_mem_format_type, last_disp_arg.nctype.type),
             name=last_disp_arg.name,
             default=last_disp_arg.default,
             argument=last_disp_arg,
@@ -331,45 +328,82 @@ def generate_static_dispatch(
     else:
         dp_sig_args = sig.arguments()
 
-    exprs = translate(dp_sig_args, target_sig.arguments(), method=method)
-    exprs_str = ', '.join(a.expr for a in exprs)
-    if f.structured_delegate is not None:
-        # TODO: for ops with structured_delegate it should check the dispatch table of
-        # the out variant instead. For now, these structured ops all have CPU/CUDA kernels
-        # so we always dispatch to the `backend`, but this could be wrong when we
-        # migrate math/default_backend ops to use structured delegate.
-        if backend_index.has_kernel(f) or backend_index.dispatch_key in STRUCTURED_DISPATCH_KEYS:
-            return f'return at::{backend_index.dispatch_key.lower()}::{name}({exprs_str});'
-        else:
-            return f'TORCH_CHECK(false, "Static dispatch does not support {name} for {backend_index.dispatch_key}.");'
+    exprs = translate(dp_sig_args, target_sig.arguments())
+    return ', '.join(a.expr for a in exprs)
 
-    if backend_index.has_kernel(f):
-        return f'return at::{backend_index.dispatch_key.lower()}::{name}({exprs_str});'
-    elif f.has_composite_explicit_autograd_kernel:
-        return f'return at::{DispatchKey.CompositeExplicitAutograd.lower()}::{name}({exprs_str});'
+
+def generate_static_dispatch_backend_call(
+    f: NativeFunction,
+    backend_index: BackendIndex,
+    name: str,
+    exprs: static_dispatch_ops_header
+) -> str:
+    return f'return at::{backend_index.dispatch_key.lower()}::{name}({exprs});'
+
+
+def generate_static_dispatch_fallback_call(
+    f: NativeFunction,
+    backend_indices: List[BackendIndex],
+    name: str,
+    exprs: static_dispatch_ops_header
+) -> str:
+    if f.has_composite_explicit_autograd_kernel:
+        return f'return at::{DispatchKey.CompositeExplicitAutograd.lower()}::{name}({exprs});'
     elif f.has_composite_implicit_autograd_kernel:
-        return f'return at::{DispatchKey.CompositeImplicitAutograd.lower()}::{name}({exprs_str});'
-    return f'TORCH_CHECK(false, "Static dispatch does not support {name} for {backend_index.dispatch_key}.");'
+        return f'return at::{DispatchKey.CompositeImplicitAutograd.lower()}::{name}({exprs});'
+    else:
+        return f"""TORCH_CHECK(false, "Static dispatch does not support {name} for\
+{', '.join([str(index.dispatch_key)for index in backend_indices])} ");"""
 
 
 def static_dispatch(
     f: NativeFunction,
     sig: DispatcherSignature,
-    *,
-    method: bool,
     backend_indices: List[BackendIndex],
 ) -> Optional[str]:
     if len(backend_indices) == 0 or f.manual_kernel_registration:
         return None
-    keys = [b for b in backend_indices if b.has_kernel(f) or f.structured_delegate is not None]
+    name = sig.name()
+    exprs_str = translate_args_disp_to_cpp(f, sig)
+
+    keys = [b for b in backend_indices if b.has_kernel(f) or (f.structured_delegate is not None and b.dispatch_key in STRUCTURED_DISPATCH_KEYS)]
     if len(keys) == 1:
-        return generate_static_dispatch(f, sig, method=method, backend_index=keys[0])
+        return generate_static_dispatch_backend_call(f, keys[0], name, exprs_str)
     elif len(keys) == 0:
-        return generate_static_dispatch(f, sig, method=method, backend_index=backend_indices[0])
-    else:
-        return f"""TORCH_CHECK(false, "Static dispatch does not support {f.func.name.unambiguous_name()} for\
-{', '.join([str(index.dispatch_key)for index in backend_indices])} as they have with multiple \
-kernels {', '.join([str(k.get_kernel(f)) for k in keys])} ");"""
+        return generate_static_dispatch_fallback_call(f, backend_indices, name, exprs_str)
+
+    tensor_opts = f.func.arguments.tensor_options
+    native_tensor_args = [
+        a.name for a in sig.arguments()
+        if isinstance(a.argument, SelfArgument) or isinstance(a.argument, Argument) and a.argument.type.is_tensor_like()
+    ]
+    tensor_args = ', '.join(native_tensor_args)
+
+    stmts = []
+    subexprs: List[str] = []
+    if tensor_opts is not None:
+        subexprs.append(f'DispatchKeySet(c10::computeDispatchKey(dtype, layout, device))')
+    if tensor_args != "":
+        subexprs.append(f'c10::detail::multi_dispatch_key_set({tensor_args})')
+    stmts.append(f"""DispatchKeySet _dk_set = {' | '.join(subexprs)};""")
+    stmts.append("""DispatchKey _dk = c10::highestPriorityBackendTypeId(_dk_set);""")
+
+    dispatch_code = []
+    for index in keys:
+        dispatch_code.append(f"""case DispatchKey::{index.dispatch_key}:""")
+        dispatch_code.append(f"""\t{generate_static_dispatch_backend_call(f, index, name, exprs_str)};""")
+
+    fallback = generate_static_dispatch_fallback_call(f, backend_indices, name, exprs_str)
+    connector = '\n\t\t'
+
+    return f"""
+    {connector.join(stmts)}
+    switch (_dk) {{
+        {connector.join(dispatch_code)}
+        default:
+            {fallback}
+    }}
+    """
 
 
 # Generates RegisterSchema.cpp.  Depending on the selector, either
@@ -473,8 +507,7 @@ static C10_NOINLINE c10::TypedOperatorHandle<{name}::schema> create_{name}_typed
 
                 if not is_redispatching_fn and len(self.static_dispatch_backend_indices) > 0:
                     # call() should go through static dispatch
-                    fn_body = static_dispatch(f, sig, method=False,
-                                              backend_indices=self.static_dispatch_backend_indices)
+                    fn_body = static_dispatch(f, sig, backend_indices=self.static_dispatch_backend_indices)
                 defns += f"""
 // aten::{f.func}
 {sig.defn(name=method_name, is_redispatching_fn=is_redispatching_fn)} {{
